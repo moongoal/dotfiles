654844dee65ac8e6a1ccb540202d3110ed6081f3
Alfredo Mungo <alfredo.mungo@openmailbox.org>
2016-05-27 14:22:17 +0200

    Improved composite manager integration

--- i3/quit.sh
+++ i3/quit.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-xmessage -buttons "Yes:0,No:1" -default No -center -timeout 5 "Exit X Session?" \
-  && i3-msg exit
+if xmessage -buttons "Yes:0,No:1" -default No -center -timeout 5 "Exit X Session?"; then
+  # Terminate composite manager
+  CMGR_PID=$(pgrep compton)
+  [[ -n $CMGR_PID ]] && kill $CMGR_PID && wait $CMGR_PID
+
+  # Quit window manager
+  i3-msg exit
+fi
